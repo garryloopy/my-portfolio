817e7878d9094ee897ba7de69f4dd848e32b8f54
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-03-12 12:41:45 -0600

    Change in layouts

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,10 @@
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Portfolio - Garry Jr Dayag",
--- app/layouts/PageLayout.js
+++ app/layouts/PageLayout.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 export default function PageLayout({ children, className }) {
   return (
     <motion.main
-      className={`min-h-screen w-full flex flex-col p-6 ${className}`}
+      className={`min-h-screen w-full flex flex-col p-6 ${className} bg-zinc-900/25`}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
     >
