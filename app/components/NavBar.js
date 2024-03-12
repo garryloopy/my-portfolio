b7a177ab721b73eee4e8394181e92158ceae7e61
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-03-12 12:41:34 -0600

    Change in navbar

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,56 +1,33 @@
 "use client";
 
 import Link from "next/link";
-import { motion } from "framer-motion";
 
 export default function NavBar() {
   return (
     <header
-      className="bg-gradient-to-tr from-[#00111C] to-[#001523]
-      h-16 flex flex-row justify-center items-center px-6 shadow-md border border-[#00111C]  fixed top-0 w-full"
+      className="bg-zinc-800/70
+      h-16 flex flex-row justify-center items-center px-6 shadow-md fixed top-0 w-full z-10 border-b border-zinc-600"
     >
-      <div className="flex flex-row justify-between items-center w-[64rem] h-full bg-opacity-100">
-        <motion.div initial={{ opacity: 0.75 }} whileHover={{ opacity: 1 }}>
-          <Link
-            className="font-bold text-2xl text-gray-50/90 hover:text-[gray-100]/90"
-            href="/"
-          >
-            Garry Jr Dayag
-          </Link>
-        </motion.div>
+      <div className="flex flex-row justify-between items-center w-[64rem] h-full">
+        <Link className="font-bold text-xl text-gray-50/90" href="/">
+          Garry Jr Dayag
+        </Link>
         <div className="flex flex-row gap-4">
-          <motion.div initial={{ opacity: 0.75 }} whileHover={{ opacity: 1 }}>
-            <Link
-              className="font-semibold text-lg text-gray-50/90 hover:text-gray-100/90"
-              href="/"
-            >
-              Home
-            </Link>
-          </motion.div>
-          <motion.div initial={{ opacity: 0.75 }} whileHover={{ opacity: 1 }}>
-            <Link
-              className="font-semibold text-lg text-gray-50/90 hover:text-gray-100/90"
-              href="/"
-            >
-              About me
-            </Link>
-          </motion.div>
-          <motion.div initial={{ opacity: 0.75 }} whileHover={{ opacity: 1 }}>
-            <Link
-              className="font-semibold text-lg text-gray-50/90 hover:text-gray-100/90"
-              href="/"
-            >
-              Projects
-            </Link>
-          </motion.div>
-          <motion.div initial={{ opacity: 0.75 }} whileHover={{ opacity: 1 }}>
-            <Link
-              className="font-semibold text-lg text-gray-50/90 hover:text-gray-100/90"
-              href="/prototyping"
-            >
-              Prototyping
-            </Link>
-          </motion.div>
+          <Link className="font-medium text-lg text-gray-50" href="/">
+            Home
+          </Link>
+          <Link className="font-medium text-lg text-gray-50" href="/">
+            About me
+          </Link>
+          <Link className="font-medium text-lg text-gray-50" href="/">
+            Projects
+          </Link>
+          <Link
+            className="font-medium text-lg text-gray-50"
+            href="/prototyping"
+          >
+            Prototyping
+          </Link>
         </div>
       </div>
     </header>
