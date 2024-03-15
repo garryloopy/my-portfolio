3c225e0bc6859e126b710ae25d918d4245b46dca
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-03-14 19:27:03 -0600

    Update in navbar

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -2,11 +2,11 @@
 
 import { IoMenu, IoClose } from "react-icons/io5";
 
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 import { animateScroll, scroller } from "react-scroll";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function NavBar() {
   const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
@@ -121,7 +121,7 @@ export default function NavBar() {
         </div>
 
         <div className="sm:hidden flex justify-center w-full h-full">
-          <div className="fixed top-4 right-4">
+          <div className="fixed top-4 right-4 z-10">
             {isHamburgerOpen ? (
               <motion.div
                 initial={{ opacity: 0 }}
@@ -148,57 +148,81 @@ export default function NavBar() {
               </motion.div>
             )}
           </div>
-          {isHamburgerOpen && (
-            <div className=" relative inset-y-16 w-full h-fit bg-zinc-950">
-              <div className=" flex flex-col items-center justify-center bg-zinc-900/45 divide-y shadow-md divide-zinc-700 border-y border-zinc-700 h-fit w-full">
-                <motion.button
-                  initial={{ opacity: 0, x: -128 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  transition={{ duration: 0.2, ease: "easeInOut" }}
-                  className="font-medium text-lg text-gray-50 w-full h-full py-3"
-                  onClick={handleScrollToTop}
-                >
-                  Home
-                </motion.button>
-                <motion.button
-                  initial={{ opacity: 0, x: -128 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  transition={{ duration: 0.2, ease: "easeInOut", delay: 0.05 }}
-                  className="font-medium text-lg text-gray-50 w-full h-full py-3"
-                  onClick={handleScrollToAboutMe}
-                >
-                  About me
-                </motion.button>
-                <motion.button
-                  initial={{ opacity: 0, x: -128 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  transition={{ duration: 0.2, ease: "easeInOut", delay: 0.1 }}
-                  className="font-medium text-lg text-gray-50 w-full h-full py-3"
-                  onClick={handleScrollToEducation}
-                >
-                  Education
-                </motion.button>
-                <motion.button
-                  initial={{ opacity: 0, x: -128 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  transition={{ duration: 0.2, ease: "easeInOut", delay: 0.15 }}
-                  className="font-medium text-lg text-gray-50 w-full h-full py-3"
-                  onClick={handleScrollToExperience}
-                >
-                  Experience
-                </motion.button>
-                <motion.button
-                  initial={{ opacity: 0, x: -128 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  transition={{ duration: 0.2, ease: "easeInOut", delay: 0.2 }}
-                  className="font-medium text-lg text-gray-50 w-full h-full py-3"
-                  onClick={handleScrollToProjects}
-                >
-                  Projects
-                </motion.button>
-              </div>
-            </div>
-          )}
+          <AnimatePresence>
+            {isHamburgerOpen && (
+              <motion.div
+                className=" relative inset-y-16 w-full h-fit bg-zinc-950 -z-10"
+                animate={{ y: 0, opacity: 1 }}
+                initial={{ y: -128, opacity: 1 }}
+                transition={{ duration: 0.25, ease: "easeInOut" }}
+                exit={{ y: -128, opacity: 0 }}
+              >
+                <div className=" flex flex-col items-center justify-center bg-zinc-900/45 divide-y shadow-md divide-zinc-700 border-y border-zinc-700 h-fit w-full">
+                  <motion.button
+                    initial={{ opacity: 0, x: -128 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.2, ease: "easeInOut" }}
+                    className="font-medium text-lg text-gray-50 w-full h-full py-3"
+                    onClick={handleScrollToTop}
+                  >
+                    Home
+                  </motion.button>
+                  <motion.button
+                    initial={{ opacity: 0, x: -128 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{
+                      duration: 0.2,
+                      ease: "easeInOut",
+                      delay: 0.05,
+                    }}
+                    className="font-medium text-lg text-gray-50 w-full h-full py-3"
+                    onClick={handleScrollToAboutMe}
+                  >
+                    About me
+                  </motion.button>
+                  <motion.button
+                    initial={{ opacity: 0, x: -128 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{
+                      duration: 0.2,
+                      ease: "easeInOut",
+                      delay: 0.1,
+                    }}
+                    className="font-medium text-lg text-gray-50 w-full h-full py-3"
+                    onClick={handleScrollToEducation}
+                  >
+                    Education
+                  </motion.button>
+                  <motion.button
+                    initial={{ opacity: 0, x: -128 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{
+                      duration: 0.2,
+                      ease: "easeInOut",
+                      delay: 0.15,
+                    }}
+                    className="font-medium text-lg text-gray-50 w-full h-full py-3"
+                    onClick={handleScrollToExperience}
+                  >
+                    Experience
+                  </motion.button>
+                  <motion.button
+                    initial={{ opacity: 0, x: -128 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{
+                      duration: 0.2,
+                      ease: "easeInOut",
+                      delay: 0.2,
+                    }}
+                    className="font-medium text-lg text-gray-50 w-full h-full py-3"
+                    onClick={handleScrollToProjects}
+                  >
+                    Projects
+                  </motion.button>
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </div>
     </header>
