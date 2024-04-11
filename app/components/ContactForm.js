ce5015abbcb6cdc661cca73d4c3490816a392a49
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-04-11 16:22:50 -0600

    Minor UI change in contact form

--- app/components/ContactForm.js
+++ app/components/ContactForm.js
@@ -112,12 +112,12 @@ export default function ContactForm({ contactFormState, onDialogClose }) {
         <div className=" flex h-max w-full flex-col items-center justify-center gap-4 bg-gradient-to-tr from-zinc-800/40 to-zinc-800/45 p-8 sm:p-16">
           <h1 className="mb-8 text-3xl font-semibold text-zinc-300">Get in touch</h1>
           {/* Label for the name input */}
-          <label className="relative flex min-h-12 w-full items-end rounded-md bg-zinc-400 focus-within:shadow-md focus-within:shadow-[#C6AB84] focus-within:ring-2 focus-within:ring-[#C6AB84] hover:cursor-text sm:min-h-16 transition-all duration-300 opacity-75 focus-within:opacity-100 hover:opacity-100">
+          <label className="relative flex min-h-12 w-full items-end rounded-md bg-zinc-300 focus-within:shadow-md focus-within:shadow-[#C6AB84] focus-within:ring-2 focus-within:ring-[#C6AB84] hover:cursor-text sm:min-h-16 transition-all duration-300 opacity-75 focus-within:opacity-100 hover:opacity-100">
             <input
               type="text"
               value={nameValue}
               onChange={handleNameChange}
-              className="sm:text-md peer h-4/6 w-full rounded-lg bg-zinc-400 px-4 pb-1 text-sm text-zinc-800 placeholder-zinc-800 outline-none sm:pb-2"
+              className="sm:text-md peer h-4/6 w-full rounded-lg bg-zinc-300 px-4 pb-1 text-sm text-zinc-800 placeholder-zinc-800 outline-none sm:pb-2"
               required
             />
             <p
@@ -128,12 +128,12 @@ export default function ContactForm({ contactFormState, onDialogClose }) {
             </p>
           </label>
           {/* Label for the email input */}
-          <label className="relative flex min-h-12 w-full items-end rounded-md bg-zinc-400 focus-within:shadow-md focus-within:shadow-[#C6AB84] focus-within:ring-2 focus-within:ring-[#C6AB84] hover:cursor-text sm:min-h-16 transition-all duration-300 opacity-75 focus-within:opacity-100 hover:opacity-100">
+          <label className="relative flex min-h-12 w-full items-end rounded-md bg-zinc-300 focus-within:shadow-md focus-within:shadow-[#C6AB84] focus-within:ring-2 focus-within:ring-[#C6AB84] hover:cursor-text sm:min-h-16 transition-all duration-300 opacity-75 focus-within:opacity-100 hover:opacity-100">
             <input
               type="text"
               value={emailValue}
               onChange={handleEmailChange}
-              className="sm:text-md peer h-4/6 w-full rounded-lg bg-zinc-400 px-4 pb-1 text-sm text-zinc-800 placeholder-zinc-800 outline-none sm:pb-2"
+              className="sm:text-md peer h-4/6 w-full rounded-lg bg-zinc-300 px-4 pb-1 text-sm text-zinc-800 placeholder-zinc-800 outline-none sm:pb-2"
               required
             />
             <p
@@ -144,12 +144,12 @@ export default function ContactForm({ contactFormState, onDialogClose }) {
             </p>
           </label>
           {/* Label for the phone number input */}
-          <label className="relative flex min-h-12 w-full items-end rounded-md bg-zinc-400 focus-within:shadow-md focus-within:shadow-[#C6AB84] focus-within:ring-2 focus-within:ring-[#C6AB84] hover:cursor-text sm:min-h-16 transition-all duration-300 opacity-75 focus-within:opacity-100 hover:opacity-100">
+          <label className="relative flex min-h-12 w-full items-end rounded-md bg-zinc-300 focus-within:shadow-md focus-within:shadow-[#C6AB84] focus-within:ring-2 focus-within:ring-[#C6AB84] hover:cursor-text sm:min-h-16 transition-all duration-300 opacity-75 focus-within:opacity-100 hover:opacity-100">
             <input
               type="text"
               value={phoneNumberValue}
               onChange={handlePhoneNumberChange}
-              className="sm:text-md peer h-4/6 w-full rounded-lg bg-zinc-400 px-4 pb-1 text-sm text-zinc-800 placeholder-zinc-800 outline-none sm:pb-2"
+              className="sm:text-md peer h-4/6 w-full rounded-lg bg-zinc-300 px-4 pb-1 text-sm text-zinc-800 placeholder-zinc-800 outline-none sm:pb-2"
               required
             />
             <p
@@ -159,23 +159,23 @@ export default function ContactForm({ contactFormState, onDialogClose }) {
               Phone number
             </p>
           </label>
-          <label className="relative flex min-h-40 w-full flex-col-reverse overflow-hidden rounded-md bg-zinc-400 focus-within:shadow-md focus-within:ring-2 focus-within:shadow-[#C6AB84] focus-within:ring-[#C6AB84] hover:cursor-text transition-all duration-300  opacity-75 focus-within:opacity-100 hover:opacity-100">
+          <label className="relative flex min-h-40 w-full flex-col-reverse overflow-hidden rounded-md bg-zinc-300 focus-within:shadow-md focus-within:ring-2 focus-within:shadow-[#C6AB84] focus-within:ring-[#C6AB84] hover:cursor-text transition-all duration-300  opacity-75 focus-within:opacity-100 hover:opacity-100">
             <textarea
               value={messageValue}
               onChange={handleMessageChange}
-              className="sm:text-md peer peer h-full w-full bg-zinc-400 p-4 text-sm text-zinc-800 placeholder-zinc-800 outline-none"
+              className="sm:text-md peer peer h-full w-full bg-zinc-300 p-4 text-sm text-zinc-800 placeholder-zinc-800 outline-none"
               rows={4}
               cols={4}
               required
             />
-            <div className="flex h-12 w-full select-none items-center justify-center bg-zinc-500/50 transition-all duration-300 peer-focus:font-semibold sm:h-20">
+            <div className="flex h-12 w-full select-none items-center justify-center bg-zinc-400/50 transition-all duration-300 peer-focus:font-semibold sm:h-20">
               <p>Message</p>
             </div>
           </label>
 
           {/* Submit button */}
           <button
-            className="h-12 w-full rounded-md bg-gradient-to-r from-zinc-400 to-zinc-500 outline-none transition-all duration-300 ease-in-out hover:bg-zinc-500 focus:shadow-md focus:shadow-[#C6AB84] focus:ring-2 focus:ring-[#C6AB84] active:bg-zinc-600  opacity-75 focus-within:opacity-100 hover:opacity-100 mt-8"
+            className="h-12 w-full rounded-md bg-gradient-to-r from-zinc-200 to-zinc-400 outline-none transition-all duration-300 ease-in-out hover:bg-zinc-500 focus:shadow-md focus:shadow-[#C6AB84] focus:ring-2 focus:ring-[#C6AB84] active:bg-zinc-600  opacity-75 focus-within:opacity-100 hover:opacity-100 mt-8"
             type="submit"
           >
             <p className="select-none font-semibold">Submit</p>
